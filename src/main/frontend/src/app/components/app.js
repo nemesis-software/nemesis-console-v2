b434a9ef8ff501389b663843e0b6a55416092df0
TeodorTunev <teodortunev@gmail.com>
2017-01-16 13:48:12 +0200

    Add support for multilanguages

--- src/main/frontend/src/app/components/app.js
+++ src/main/frontend/src/app/components/app.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import { componentRequire } from '../utils/require-util'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import counterpart from 'counterpart';
+injectTapEventPlugin();
+
+counterpart.registerTranslations('en', require('../../locales/en'));
+counterpart.registerTranslations('bg', require('../../locales/bg'));
+
+counterpart.setLocale('bg');
 
 var NavigationTree = componentRequire('app/components/navigation-tree', 'navigation-tree');
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        Hello from new console!!!
-        <NavigationTree />
-      </div>
+      <MuiThemeProvider>
+        <div>
+            <button onClick={() => {counterpart.setLocale('bg')}}>BG</button>
+            <button onClick={() => {counterpart.setLocale('en')}}>EN</button>
+            <NavigationTree />
+        </div>
+      </MuiThemeProvider>
+
     );
   }
 }
--- src/main/frontend/src/app/components/navigation-tree.js
+++ src/main/frontend/src/app/components/navigation-tree.js
@@ -1,29 +1,53 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {List, ListItem} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
 import CONFIG from '../../CONFIG'
+import Translate from 'react-translate-component';
 
+var self;
 export default class NavigationTree extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {treeData: [], filteredData: []};
+    self = this;
+  }
+
   componentWillMount() {
-    var url = CONFIG.baseUrl + 'backend/navigation';
-    var nemesisToken = document.getElementById('token').getAttribute('value');
+    let url = CONFIG.baseUrl + 'backend/navigation';
+    let nemesisToken = document.getElementById('token').getAttribute('value');
     axios.get(url, {
       headers: {'X-Nemesis-Token': nemesisToken}
     }).then(result => {
-      console.log(result.data);
+      this.setState({treeData: result.data, filteredData: result.data});
+      console.log(this.state);
     })
   }
 
-  render() {
-    if (!this.props.data) {
-      return (
-        <div></div>
-      )
-    }
+  logger() {
+    console.log('form base tree');
+  }
 
+  render() {
+    this.logger();
     return (
-      <div className="child">
-        {this.props.data.map(item => <div key={item.name}>{item.name}</div>)}
-      </div>
+      <List>
+        <Subheader><Translate component="p" content="main.navigation"/></Subheader>
+        {this.state.filteredData.map(this.listItemRenderer)}
+      </List>
+    );
+  }
+
+  listItemRenderer(item, index) {
+    let children = item.children || item.childNodes;
+    return (
+      <ListItem
+                key={index}
+                primaryText={<Translate component="p" content={'main.' + item.text} />}
+                primaryTogglesNestedList={true}
+                nestedItems={children.map(self.listItemRenderer)}
+
+      />
     );
   }
 }
