a18ae42cdea0ce8a72b031c22b0f3f154a33013b
TeodorTunev <teodortunev@gmail.com>
2017-06-21 16:33:10 +0300

    Fix entity navigation overflow

--- src/main/frontend/src/app/components/entities-navigation/entities-navigation.js
+++ src/main/frontend/src/app/components/entities-navigation/entities-navigation.js
@@ -6,28 +6,43 @@ import _ from 'lodash';
 
 import { componentRequire } from '../../utils/require-util';
 
+import ReactDOM from 'react-dom'
+
 let EntitiesNavigationItem = componentRequire('app/components/entities-navigation/entity-navigation-item', 'entity-navigation-item');
 
 export default class EntitiesNavigation extends Component {
   constructor(props) {
     super(props);
-    this.state = {groupedEntities: []};
+    this.state = {groupedEntities: [], containerHeight: '44'};
+    this.navRef = null;
   }
 
   componentWillReceiveProps(nextProps) {
-    this.setState({groupedEntities: this.getGroupedEntities(nextProps.entities)});
+    this.setState({...this.state, groupedEntities: this.getGroupedEntities(nextProps.entities)});
+  }
+
+  componentDidUpdate() {
+    if (this.navRef) {
+      let domNode = ReactDOM.findDOMNode(this.navRef);
+      let nodeHeight = domNode.clientHeight;
+      if (this.state.containerHeight !== nodeHeight) {
+        this.setState({...this.state, containerHeight: nodeHeight});
+      }
+    }
   }
 
   render() {
     return (
-      <Nav bsStyle="pills" className="nav nav-pills entity-navigation">
-        {_.map(this.state.groupedEntities, (value, key) =>
-          <EntitiesNavigationItem key={key} entityId={key}
-                                  entities={value.reverse()}
-                                  onEntityWindowClose={this.props.onEntityWindowClose}
-                                  onNavigationItemClick={this.props.onNavigationItemClick}/>
-        )}
-      </Nav>
+      <div style={this.getContainerStyle()}>
+        <Nav ref={el => this.navRef = el} bsStyle="pills" className="nav nav-pills entity-navigation">
+          {_.map(this.state.groupedEntities, (value, key) =>
+            <EntitiesNavigationItem key={key} entityId={key}
+                                    entities={value.reverse()}
+                                    onEntityWindowClose={this.props.onEntityWindowClose}
+                                    onNavigationItemClick={this.props.onNavigationItemClick}/>
+          )}
+        </Nav>
+      </div>
     )
   }
 
@@ -40,5 +55,14 @@ export default class EntitiesNavigation extends Component {
     return _.fromPairs(
       _.map(sortedKeys, key => [key, groupedEntities[key]])
     )
+  }
+
+  getContainerStyle() {
+    let style = {
+      width: '100%',
+      height: this.state.containerHeight + 'px'
+    };
+
+    return style;
   }
 }
