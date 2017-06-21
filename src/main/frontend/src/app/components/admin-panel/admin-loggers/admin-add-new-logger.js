3c3ea2fca7edd3f068b815fe7b3770d7a201f3a0
TeodorTunev <teodortunev@gmail.com>
2017-06-21 12:37:01 +0300

    Add admin logger functionality

---
+++ src/main/frontend/src/app/components/admin-panel/admin-loggers/admin-add-new-logger.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import AdminExpandable from '../admin-expandable';
+import Select from 'react-select';
+
+export default class AdminAddNewLogger extends AdminExpandable {
+  constructor(props) {
+    super(props);
+    this.state = {isExpanded: true, newLoggerName: '', newLoggerLevel: null};
+  }
+
+  getExpandedContent() {
+    return (
+      <div className="add-new-logger">
+        <div className="input-group new-logger-fields">
+          <input type="text"
+                 style={{height: '36px'}}
+                 value={this.state.newLoggerName}
+                 placeholder="Logger name"
+                 className="form-control"
+                 onChange={this.onNewLoggerNameChange.bind(this)}/>
+          <div style={{display: 'table-cell', verticalAlign: 'top'}}>
+            <Select disabled={this.props.readOnly}
+                    value={{value: this.state.newLoggerLevel, label: this.state.newLoggerLevel }}
+                    onChange={(item) => this.handleNewLoggerLevelChange(item)}
+                    options={this.getOptions()}/>
+          </div>
+        </div>
+        <div className="button-container">
+          <button disabled={!this.state.newLoggerLevel || !this.state.newLoggerName} className="btn btn-default" onClick={this.handleOnAddLoggerClick.bind(this)}>Add Logger</button>
+        </div>
+      </div>
+    );
+  }
+
+  getHeaderText() {
+    return 'Add new logger';
+  }
+
+  onNewLoggerNameChange(ev) {
+    this.setState({...this.state, newLoggerName: ev.target.value});
+  }
+
+  getOptions() {
+    return this.props.levels.map((level) => {
+      return {value: level, label: level}
+    });
+  }
+
+  handleNewLoggerLevelChange(item) {
+    let level = item && item.value;
+    this.setState({...this.state, newLoggerLevel: level});
+  }
+
+  handleOnAddLoggerClick() {
+    this.props.addNewLogger(this.state.newLoggerLevel, {name: this.state.newLoggerName}).then(() => {
+      this.setState({...this.state, newLoggerName: '', newLoggerLevel: null})
+    });
+  }
+}
--- src/main/frontend/src/app/components/admin-panel/admin-loggers/admin-loggers.js
+++ src/main/frontend/src/app/components/admin-panel/admin-loggers/admin-loggers.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import PlatformApiCall from '../../../services/platform-api-call';
 import _ from 'lodash';
+import counterpart from 'counterpart';
+import AddNewLogger from './admin-add-new-logger'
+
 
 export default class AdminLoggers extends Component {
   constructor(props) {
     super(props);
-    this.state = {levels: [], loggers: {}, filteredLoggers: {}};
+    this.state = {levels: [], loggers: {}, filteredLoggers: [], searchInput: ''};
   }
 
   componentWillMount() {
@@ -15,8 +18,16 @@ export default class AdminLoggers extends Component {
   render() {
     return (
       <div className="admin-loggers">
+        <div className="input-group">
+          <input type="text"
+                 placeholder={counterpart.translate('main.Filter...', {fallback: 'Filter'})}
+                 className="form-control"
+                 onChange={this.onFilterChange.bind(this)}/>
+          <span className="input-group-addon"><i className="fa fa-search" /></span>
+        </div>
+        <AddNewLogger addNewLogger={this.setLoggerLevel.bind(this)} levels={this.state.levels}/>
         <div className="display-table" style={{width: '100%'}}>
-          {this.getLoggers().map(logger => {
+          {this.state.filteredLoggers.map(logger => {
             return <div className="display-table-row" key={logger.name}>
               <div className="display-table-cell">{logger.name}</div>
               <div className="display-table-cell" style={{textAlign: 'right'}}>{this.getLoggerButtons(logger)}</div>
@@ -32,32 +43,40 @@ export default class AdminLoggers extends Component {
       <div className="btn-group">
         {this.state.levels.map((level, index) => {
           return <button className={'btn btn-default ' + level.toLowerCase() + (level === logger.level ? ' current-level' :'')}
-                         key={index} onClick={() => this.changeLoggerLevel(level, logger)}>{level}</button>
+                         key={index} onClick={() => this.setLoggerLevel(level, logger)}>{level}</button>
         })}
       </div>
     )
   }
 
-  getLoggers() {
+  setLoggerLevel(level, logger) {
+    if (level === logger.level) {
+      return;
+    }
+
+    return PlatformApiCall.post(`loggers/${logger.name}`, {configuredLevel: level}).then(this.populateLoggers.bind(this));
+  }
+
+  populateLoggers() {
+    return PlatformApiCall.get('loggers').then(result => {
+      this.setState({levels: _.values(result.data.levels), loggers: result.data.loggers, filteredLoggers: this.getFilteredLoggers(result.data.loggers, this.state.searchInput)})
+    });
+  }
+
+  getFilteredLoggers(loggers, searchInput) {
     let result = [];
-    _.forIn(this.state.filteredLoggers, (value, key) => {
-      result.push({name: key, level: value.configuredLevel})
+    _.forIn(loggers, (value, key) => {
+      if (!searchInput || (key.toLowerCase().indexOf(searchInput.toLowerCase()) > -1)) {
+        result.push({name: key, level: value.configuredLevel})
+      }
     });
 
     return result;
   }
 
-  changeLoggerLevel(level, logger) {
-    if (level === logger.level) {
-      return;
-    }
-
-    PlatformApiCall.post(`loggers/${logger.name}`, {configuredLevel: level}).then(this.populateLoggers.bind(this));
+  onFilterChange(ev) {
+    let searchValue = ev.target.value;
+    this.setState({...this.state, searchInput:searchValue , filteredLoggers: this.getFilteredLoggers(this.state.loggers, searchValue)});
   }
 
-  populateLoggers() {
-    PlatformApiCall.get('loggers').then(result => {
-      this.setState({levels: _.values(result.data.levels), loggers: result.data.loggers, filteredLoggers: result.data.loggers})
-    });
-  }
 }
