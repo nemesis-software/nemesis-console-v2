442d3343d1d95a7761e163e20d74982039afde46
teo <teodortunev@gmail.com>
2017-08-23 13:50:14 +0300

    Fix missspelling component name

--- src/main/frontend/src/custom_files/filters/transaction-filter.js
+++ src/main/frontend/src/custom_files/filters/transaction-filter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-import FilterNumberField from '../../app/components/entity-window/entities-viewer/entities-filter/filter-fields/filter-enum-field/filter-enum-field'
+import FilterEnumField from '../../app/components/entity-window/entities-viewer/entities-filter/filter-fields/filter-enum-field/filter-enum-field'
 import BaseCustomFilter from './base-custom-filter';
 
 export default class TransactionFilter extends BaseCustomFilter {
@@ -12,7 +12,7 @@ export default class TransactionFilter extends BaseCustomFilter {
   render() {
     return (
       <div style={this.props.style}>
-        <FilterNumberField readOnly={true} defaultValue={'AUTHORIZE'} onFilterChange={this.onFilterChange.bind(this)} filterItem={this.getTranscationItem()} />
+        <FilterEnumField readOnly={false} defaultValue={'AUTHORIZE'} onFilterChange={this.onFilterChange.bind(this)} filterItem={this.getTranscationItem()} />
         <div style={{padding: '10px 0'}} ><button className="btn btn-default" onClick={this.onSearchButtonClick.bind(this)}>Search</button></div>
       </div>
     )
