9d42e4fb67cab1e5ac554c20403d7bc746675e98
teo <teodortunev@gmail.com>
2017-12-13 17:47:58 +0200

    Add some styles

--- src/main/frontend/src/app/components/point-of-sale/bill-panel/bill-control-buttons/bill-control-buttons.js
+++ src/main/frontend/src/app/components/point-of-sale/bill-panel/bill-control-buttons/bill-control-buttons.js
@@ -11,29 +11,32 @@ export default class BillControlButtons extends Component {
   render() {
     return (
       <div className="bill-control-buttons">
-        <div className="payment-process-button">Payment</div>
+        <div className="payment-process-button">
+          <div><i className="material-icons">attach_money</i></div>
+          Payment
+        </div>
         <div className="number-pad-container">
           <table>
             <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
+              <td className="number-buttons">1</td>
+              <td className="number-buttons">2</td>
+              <td className="number-buttons">3</td>
               <td rowSpan={2}>Qty</td>
             </tr>
             <tr>
-              <td>4</td>
-              <td>5</td>
-              <td>6</td>
+              <td className="number-buttons">4</td>
+              <td className="number-buttons">5</td>
+              <td className="number-buttons">6</td>
             </tr>
             <tr>
-              <td>7</td>
-              <td>8</td>
-              <td>9</td>
-              <td rowSpan={2}>Back</td>
+              <td className="number-buttons">7</td>
+              <td className="number-buttons">8</td>
+              <td className="number-buttons">9</td>
+              <td rowSpan={2}><i className="material-icons">backspace</i></td>
             </tr>
             <tr>
-              <td colSpan={2}>0</td>
-              <td>.</td>
+              <td  className="number-buttons" colSpan={2}>0</td>
+              <td className="number-buttons">.</td>
             </tr>
           </table>
         </div>
