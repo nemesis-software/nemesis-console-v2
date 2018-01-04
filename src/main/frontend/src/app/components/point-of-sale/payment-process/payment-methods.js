b882bcbed91ca74e0ba06cdc7d3537152f066eff
teo <teodortunev@gmail.com>
2018-01-04 15:20:16 +0200

    Add payment moethods

--- src/main/frontend/src/app/components/point-of-sale/payment-process/payment-methods.js
+++ src/main/frontend/src/app/components/point-of-sale/payment-process/payment-methods.js
@@ -3,8 +3,7 @@ import React, {Component} from 'react';
 export default class PaymentMethods extends Component {
   constructor(props) {
     super(props);
-    this.state = {payedAmount: 0};
-
+    this.state = {payedAmount: ''};
   }
 
   render() {
@@ -13,7 +12,7 @@ export default class PaymentMethods extends Component {
         <div className="payment-methods-header">
           <div className="pos-button back-button" onClick={() => this.props.setIsPaymentProcess(false)}>Back</div>
           <div className="payment-methods-header-text">Payment</div>
-          <div className="pos-button validate-button" onClick={() => this.props.setIsPaymentProcess(false)}>Validate</div>
+          <div className="pos-button validate-button" onClick={() => this.props.onFinalizePayment(this.state.payedAmount)}>Validate</div>
         </div>
         <div style={{height: '100%'}}>
           <div className="payment-types">
@@ -34,7 +33,7 @@ export default class PaymentMethods extends Component {
                 <tbody>
                 <tr>
                   <td className="blue-td">{this.props.totalPrice} $</td>
-                  <td className="payed-number">500.00$</td>
+                  <td className="payed-number">{this.getActualAmountValue()}</td>
                   <td className="blue-td">161.00$</td>
                 </tr>
                 <tr>
@@ -47,25 +46,25 @@ export default class PaymentMethods extends Component {
               <table>
                 <tbody>
                 <tr>
-                  <td className="number-buttons pos-button">1</td>
-                  <td className="number-buttons pos-button">2</td>
-                  <td className="number-buttons pos-button">3</td>
-                  <td className="number-buttons pos-button" rowSpan={2}>C</td>
+                  <td className="number-buttons pos-button" onClick={() => this.onKeyboardButtonClick(1)}>1</td>
+                  <td className="number-buttons pos-button" onClick={() => this.onKeyboardButtonClick(2)}>2</td>
+                  <td className="number-buttons pos-button" onClick={() => this.onKeyboardButtonClick(3)}>3</td>
+                  <td className="number-buttons pos-button" rowSpan={2} onClick={() => this.onKeyboardButtonClick('clear')}>C</td>
                 </tr>
                 <tr>
-                  <td className="number-buttons pos-button">4</td>
-                  <td className="number-buttons pos-button">5</td>
-                  <td className="number-buttons pos-button">6</td>
+                  <td className="number-buttons pos-button" onClick={() => this.onKeyboardButtonClick(4)}>4</td>
+                  <td className="number-buttons pos-button" onClick={() => this.onKeyboardButtonClick(5)}>5</td>
+                  <td className="number-buttons pos-button" onClick={() => this.onKeyboardButtonClick(6)}>6</td>
                 </tr>
                 <tr>
-                  <td className="number-buttons pos-button">7</td>
-                  <td className="number-buttons pos-button">8</td>
-                  <td className="number-buttons pos-button">9</td>
-                  <td className="pos-button" rowSpan={2}><i className="material-icons">backspace</i></td>
+                  <td className="number-buttons pos-button" onClick={() => this.onKeyboardButtonClick(7)}>7</td>
+                  <td className="number-buttons pos-button" onClick={() => this.onKeyboardButtonClick(8)}>8</td>
+                  <td className="number-buttons pos-button" onClick={() => this.onKeyboardButtonClick(9)}>9</td>
+                  <td className="pos-button" rowSpan={2}><i className="material-icons" onClick={() => this.onKeyboardButtonClick('delete')}>backspace</i></td>
                 </tr>
                 <tr>
-                  <td className="number-buttons pos-button" colSpan={2}>0</td>
-                  <td className="number-buttons pos-button">.</td>
+                  <td className="number-buttons pos-button" colSpan={2} onClick={() => this.onKeyboardButtonClick(0)}>0</td>
+                  <td className="number-buttons pos-button" onClick={() => this.onKeyboardButtonClick('dot')}>.</td>
                 </tr>
                 </tbody>
               </table>
@@ -74,5 +73,41 @@ export default class PaymentMethods extends Component {
         </div>
       </div>
     )
+  }
+
+  onKeyboardButtonClick(value) {
+    if (value === 'clear') {
+      this.setState({...this.state, payedAmount: ''});
+      return;
+    }
+
+    if (value === 'delete' && this.state.payedAmount.length > 0) {
+      let amountActual = this.state.payedAmount;
+      amountActual = amountActual.substring(0, amountActual.length - 1);
+      this.setState({...this.state, payedAmount: amountActual});
+      return;
+    }
+
+    if (value === 'dot' && this.state.payedAmount.indexOf('.') === -1) {
+      let amountActual = this.state.payedAmount;
+      amountActual += '.';
+      this.setState({...this.state, payedAmount: amountActual});
+      return;
+    }
+
+    if (isFinite(value)) {
+      let amountActual = this.state.payedAmount;
+      if (amountActual.indexOf('.') === -1) {
+        amountActual += value;
+        this.setState({...this.state, payedAmount: amountActual});
+      } else if (amountActual.split('.')[1].length < 2) {
+        amountActual += value;
+        this.setState({...this.state, payedAmount: amountActual});
+      }
+    }
+  }
+
+  getActualAmountValue() {
+    return Number(this.state.payedAmount).toFixed(2);
   }
 }
--- src/main/frontend/src/app/components/point-of-sale/payment-process/payment-process.js
+++ src/main/frontend/src/app/components/point-of-sale/payment-process/payment-process.js
@@ -1,23 +1,28 @@
 import React, {Component} from 'react';
 
 import PaymentMethods from './payment-methods';
+import PaymentReceipt from './payment-receipt';
 
 export default class PaymentProcess extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {isFinalizePayment: false};
   }
 
   render() {
     return (
       <div className="payment-process">
-        <PaymentMethods onFinalizePayment={this.onFinalizePayment.bind(this)} totalPrice={this.props.cart.totalPrice} setIsPaymentProcess={this.props.setIsPaymentProcess}/>
+        {this.state.isFinalizePayment ?
+          <PaymentReceipt totalPayed={this.state.totalPayed} cart={this.props.cart}/> :
+          <PaymentMethods onFinalizePayment={this.onFinalizePayment.bind(this)} totalPrice={this.props.cart.totalPrice}
+                          setIsPaymentProcess={this.props.setIsPaymentProcess}/>
+        }
 
       </div>
     )
   }
 
   onFinalizePayment(totalMoneyPayed) {
-
+    this.setState({isFinalizePayment: true, totalPayed: totalMoneyPayed});
   }
 }
