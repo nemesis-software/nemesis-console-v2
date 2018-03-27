749e6159ea4ee55b2e69ac85014a8ceee99e0ae5
teo <teodortunev@gmail.com>
2018-03-27 13:59:59 +0300

    Add new version of migration service

--- src/main/frontend/src/app/services/migration-service.js
+++ src/main/frontend/src/app/services/migration-service.js
@@ -10,9 +10,9 @@ export default class MigrationService {
     console.log(data);
   }
 
-  static printProprtyConfig(entityData) {
+  static printProprtyConfig(entityData, searchData) {
     let data = '';
-    data += 'propertyConfig,code,name,fieldLabel,insertable,updatable,required,weight,xtype,section,sectionWeight,entity\n';
+    data += 'propertyConfig,code,name,fieldLabel,insertable,updatable,required,weight,xtype,section,sectionWeight,searchable,entity\n';
     _.forIn(entityData, (value, key) => {
       for (let i = 0; i < value.sections.length; i++) {
         let section = value.sections[i];
@@ -20,7 +20,11 @@ export default class MigrationService {
         for (let j = 0; j < section.items.length; j++) {
           let item = section.items[j];
           let itemWeight = j * 10;
-          data += `,${key}-${item.name},${item.name.replace('entity-', '')},${item.fieldLabel},${!item.readOnly},${!item.readOnly},${item.required},${itemWeight},${item.xtype},${section.title},${sectionWeight},${key}\n`
+          let searchable = false;
+          if (searchData[key] && _.some(searchData[key].filter, {name: item.name})) {
+            searchable = true;
+          }
+          data += `,${key}-${item.name},${item.name.replace('entity-', '')},${item.fieldLabel},${!item.readOnly},${!item.readOnly},${item.required},${itemWeight},${item.xtype},${section.title},${sectionWeight},${searchable},${key}\n`
         }
       }
     });
