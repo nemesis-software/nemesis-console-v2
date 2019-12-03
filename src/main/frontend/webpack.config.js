843443213b729b9352f7aadb2f0b1c208bd97f0e
deshi9012 <ilia.bojadzhiev@gmail.com>
2019-12-03 14:53:52 +0200

    switch from uglify to tester,some changes to webpack.confog.js and webpack.prod.js

--- src/main/frontend/webpack.config.js
+++ src/main/frontend/webpack.config.js
@@ -1,110 +1,117 @@
 const path = require('path');
 
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: path.join(__dirname, '../webapp/resources'),
-    publicPath: 'resources/',
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        exclude: path.join(__dirname, 'node_modules'),
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-1']
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.less$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'less-loader'
-        ]
-      },
-      {
-        test: /\.png$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 100000
-            }
-          }
-        ]
-      },
-      {
-        test: /\.jpg$/,
-        use: [
-          {
-            loader: 'file-loader'
-          }
-        ]
-      },
-      {
-        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 100000,
-              mimetype: 'application/font-woff'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 100000,
-              mimetype: 'application/octet-stream'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader'
-          }
-        ]
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 100000,
-              mimetype: 'image/svg+xml'
-            }
-          }
-        ]
-      }
-    ]
-  },
-  resolve: {
-    alias: {
-      source: path.join(__dirname, 'src'),
-      customFiles: path.join(__dirname, 'src/custom_files'),
-      localesDir: path.join(__dirname, 'src/locales'),
-      servicesDir: path.join(__dirname, 'src/app/services')
-    },
-    extensions: ['.js', '.jsx']
-  }
+	entry: [
+		'./src/index.js'
+	],
+	output: {
+		path: path.join(__dirname, '../webapp/resources'),
+		publicPath: 'resources/',
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			// {
+			//   exclude: path.join(__dirname, 'node_modules'),
+			//   loader: 'babel-loader',
+			//   query: {
+			//     presets: ['react', 'es2015', 'stage-1']
+			//   }
+			// },
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.less$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'less-loader'
+				]
+			},
+			{
+				test: /\.png$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 100000
+						}
+					}
+				]
+			},
+			{
+				test: /\.jpg$/,
+				use: [
+					{
+						loader: 'file-loader'
+					}
+				]
+			},
+			{
+				test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 100000,
+							mimetype: 'application/font-woff'
+						}
+					}
+				]
+			},
+			{
+				test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 100000,
+							mimetype: 'application/octet-stream'
+						}
+					}
+				]
+			},
+			{
+				test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{
+						loader: 'file-loader'
+					}
+				]
+			},
+			{
+				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 100000,
+							mimetype: 'image/svg+xml'
+						}
+					}
+				]
+			}
+		]
+	},
+	resolve: {
+		alias: {
+			source: path.join(__dirname, 'src'),
+			customFiles: path.join(__dirname, 'src/custom_files'),
+			localesDir: path.join(__dirname, 'src/locales'),
+			servicesDir: path.join(__dirname, 'src/app/services')
+		},
+		extensions: ['.js', '.jsx']
+	}
 };
